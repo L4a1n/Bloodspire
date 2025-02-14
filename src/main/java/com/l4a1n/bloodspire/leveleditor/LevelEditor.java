7530320bd3fa93cfd9dde38e844d34d3a124544f
L4a1n <150847641+L4a1n@users.noreply.github.com>
2025-02-14 06:58:56 +0100

    Create LevelEditor.java

---
+++ src/main/java/com/l4a1n/bloodspire/leveleditor/LevelEditor.java
@@ -0,0 +1,13 @@
+package com.l4a1n.bloodspire.leveleditor;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class LevelEditor extends Application{
+    @Override
+    public void start(Stage stage){
+        
+    }
+}
